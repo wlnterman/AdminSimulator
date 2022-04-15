1202e7c6eefd74d9d82eadeda54fabdb538fce52
unknown <shisovvlad@gmail.com>
2022-04-15 14:13:13 +0300

    3 - postgre added

--- AdminSimulator/Data/ApplicationDbContext.cs
+++ AdminSimulator/Data/ApplicationDbContext.cs
@@ -14,7 +14,19 @@ namespace AdminSimulator.Data
             : base(options)
         {
             //Database.EnsureDeleted();
-            //Database.EnsureCreated();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("" +
+                "Host=ec2-52-30-67-143.eu-west-1.compute.amazonaws.com;" +
+                "Port=5432;" +
+                "Database=dehep17tvubrp5;" +
+                "Username=gxsjgrvjkfhlhz;" +
+                "Password=abfbc336ffd5c3c0a84ce64b21ff3a0b2399dd480d70d8d6777ccb5f6ff6973e;" +
+                "sslmode=Require;" +
+                "Trust Server Certificate=true;");
+            //optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=usersdb;Username=postgres");
         }
         public ApplicationDbContext()
             : base()
